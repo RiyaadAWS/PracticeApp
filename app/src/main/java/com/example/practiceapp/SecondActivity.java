3fa9537af318b496ff8b56dc9417db57e0f7fb9c
Riyaadaws <riyaadryklief92@gmail.com>
2021-03-10 22:58:33 +0200

    New Activities added

--- app/src/main/java/com/example/practiceapp/SecondActivity.java
+++ app/src/main/java/com/example/practiceapp/SecondActivity.java
@@ -1,4 +1,32 @@
 package com.example.practiceapp;
 
-public class SecondActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class SecondActivity extends AppCompatActivity {
+
+    ImageButton button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        button = findViewById(R.id.btnb);
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        button = findViewById(R.id.btnf);
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+            startActivity(intent);
+        });
+
+
+
+    }
 }
--- app/src/main/java/com/example/practiceapp/ThirdActivity.java
+++ app/src/main/java/com/example/practiceapp/ThirdActivity.java
@@ -1,4 +1,14 @@
 package com.example.practiceapp;
 
-public class ThirdActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
 }
